4c0b1dda62b262dd19c4f56559081d2b265ce381
Jonathan Tavares <jtavares.codes@gmail.com>
2016-03-28 04:57:39 +0000

    fixing issues on codacy

--- src.js
+++ src.js
@@ -1,39 +1,39 @@
-"use strict";
+'use strict';
 
 class ImpossiblePromise {
 
-    constructor (fn){
+    constructor(fn) {
         this._stack = [];
         this._data  = [];
         this._done = false;
-        this._ups = function(){/* noop! */};
+        this._ups = function() {/* noop! */};
 
-        if(!fn){
-          throw new Error("ImpossiblePromise(:Function) requires a function to start a sequence");
+        if (typeof(fn) !== 'function') {
+          throw new Error('ImpossiblePromise(:Function) requires a function to start a sequence');
         }
 
-        process.nextTick(function() {
+        process.nextTick(() => {
             this._promise = new Promise(fn).catch(this._ups);
-        }.bind(this));
+        });
     }
 
-    then (fn){
-        if(this._done===true){
+    then(fn) {
+        if (this._done===true) {
             return new ImpossiblePromise(fn);
         }
 
-        process.nextTick(function() {
-            this._promise = this._promise.then((value) => {
+        process.nextTick( () => {
+            this._promise = this._promise.then( value => {
                 this._data.push(value);
-                return new Promise((done,err) => fn.apply(null, [done, err, value])).catch(this._ups);
+                return new Promise((done, err) => fn.apply(null, [done, err, value])).catch(this._ups);
             });
             this._stack.push(this._promise);
-        }.bind(this));
+        });
         return this;
     }
 
-    done (fn){
-        this.then((done) => {
+    done(fn) {
+        this.then( done => {
             fn.apply(null, this._data);
             done();
         });
@@ -42,7 +42,7 @@ class ImpossiblePromise {
         return this;
     }
 
-    error (fn){
+    error(fn) {
         this._ups = fn;
         return this;
     }
